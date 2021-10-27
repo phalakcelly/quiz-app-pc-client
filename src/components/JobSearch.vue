fd5bb9827283d932e1379ac6c712c993ebeca652
Phalak Celly <88779380+phalakcelly@users.noreply.github.com>
2021-10-27 21:35:57 +0530

    flex wrap jobs

--- src/components/JobSearch.vue
+++ src/components/JobSearch.vue
@@ -10,7 +10,7 @@
               {{ job.description }}
             </p>
             <a :href="job.detail_url" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
+            
           </div>
         </div>
       </div>
