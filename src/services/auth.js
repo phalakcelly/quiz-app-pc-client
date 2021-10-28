81321efffd340360f682d399af7118fe6f657295
Phalak Celly <88779380+phalakcelly@users.noreply.github.com>
2021-10-28 12:12:23 +0530

    login authentication

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,13 +2,15 @@ import axios from 'axios';
 
 import { successHandler, errorHandler } from './helper';
 
+
 export const login = ( credentials ) => {
     return axios.post(
                 `https://quiz-app-pc.herokuapp.com/auth/login`,
                 credentials,
                 {
                     headers: {
-                        'Content-Type': 'application/json'
+                        'Content-Type': 'application/json',
+                        'Authorization': localStorage.getItem( 'token' )
                     }
                 }
             )
