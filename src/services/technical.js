e79041d1b5da2bdd7c95d18612f0f4d7bf643eb8
Phalak Celly <88779380+phalakcelly@users.noreply.github.com>
2021-10-28 12:26:37 +0530

    flex wrap jobs

--- src/services/technical.js
+++ src/services/technical.js
@@ -7,7 +7,13 @@ const getques =async () => {
     console.log('inside fxn')
 
   const res=await axios.get(
-        `https://quiz-app-pc.herokuapp.com/test/technical`,   
+        `https://quiz-app-pc.herokuapp.com/test/technical`, 
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem( 'token' )
+            }
+        }    
        
     )
     console.log('response in users',res);
@@ -21,7 +27,13 @@ const resultsubmitandcheck=async (submitted_result)=>{
 
 const accuired_result=await axios.post(
     `https://quiz-app-pc.herokuapp.com/test/technical/result`,
-    submitted_result   
+    submitted_result   ,
+    {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.getItem( 'token' )
+        }
+    }  
    
 )
 
